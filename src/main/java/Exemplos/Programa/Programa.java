a2830da242dad6aa60d1983d5f772a3d3bc99a0d
fpv031 <feric5719@gmail.com>
2025-02-27 19:17:15 -0300

    testando algo

--- src/main/java/Exemplos/Programa/Programa.java
+++ src/main/java/Exemplos/Programa/Programa.java
@@ -1,4 +1,37 @@
-package Exemplos.Atividade;
+package Exemplos.Programa;
+
+import Exemplos.modelo.triangulo;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Programa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        triangulo x, y;
+        x = new triangulo();
+        y = new triangulo();
+
+        System.out.println("Enter the measures of triangle X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle x: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.println("Triangle X area: " + areaX);
+        System.out.println("Triangle y area: " + areaY);
+
+        System.out.println(x.verificadormaior(x,y));
+        sc.close();
+    }
 }
+
+
